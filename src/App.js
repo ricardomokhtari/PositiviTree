4a312a11f30ea9757e4adf9425d2d78ecc2f57fd
Ricardo Mokhtari <ricardomokhtari@gmail.com>
2020-02-29 13:54:01 +0000

    added plant

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Example from './components/ReactMic.jsx';
+import Plant from './unnamed.png';
 
 function App() {
   return (
     <React.Fragment>
       <div className = "global">
+          <img src = {Plant}></img>
           <Example/>
       </div>
     </React.Fragment>
