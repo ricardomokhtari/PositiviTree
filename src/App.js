bf1bbc6ad9d680015236eee2ad053561f88cba64
Ricardo Mokhtari <ricardomokhtari@gmail.com>
2020-02-29 18:41:09 +0000

    many many many changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
-import Example from './components/ReactMic.jsx';
 import Stage1 from './stage 1.jpg';
 import Stage2 from './stage 2.jpg';
-import PropTypes from "prop-types";
+import Stage3 from './stage 3.jpg';
 import SpeechRecognition from "react-speech-recognition";
-import { ReactMic } from 'react-mic';
 
 
 const options = {
@@ -15,14 +13,38 @@ const options = {
 
 class Dictaphone extends Component {
   state = {
-    sentiment: ""
+    sentiment: "",
+    image: Stage1,
+    level: 0
+  }
+
+  constructor(props){
+    super(props)
+    this.updatePlant = this.updatePlant.bind(this)
+  }
+
+  async updatePlant(){
+    let increment = this.state.sentiment;
+    increment = parseInt(Number(increment));
+    const newLevel = this.state.level + increment;
+    switch(newLevel){
+      case 0:
+        this.setState({image: Stage1})
+        break;
+      case 1:
+        this.setState({image: Stage2})
+        break;
+      case 2:
+        this.setState({image: Stage3})
+        break;
+    }
+    await this.setState({level: newLevel})
   }
 
   handleSend(transcript) {
     var xhr = new XMLHttpRequest()
     xhr.addEventListener('load', () => {
       // update the state of the component with the result here
-      console.log(xhr.responseText) // for debugging
       const text = xhr.responseText // extract value returned from chatbot.py
       this.setState({sentiment: text})   // update state with this value
     })
@@ -38,9 +60,13 @@ class Dictaphone extends Component {
     return (
       <React.Fragment>
         <div className = "global">
-          <img className = "images" src = {Stage1}></img>
-          <button onClick={startListening} className = "btn btn-success" type="button">Start</button>
-          <button onClick={stopListening, resetTranscript, () => this.handleSend(finalTranscript)} className = "btn btn-success" type="button">Stop</button>
+          <img className = "images" src = {this.state.image}></img>
+          <div className="buttons">
+            <button onClick={startListening} className = "btn btn-success" type="button">Start</button>
+            <div className = "button-left">
+              <button onClick={() => {stopListening(); this.updatePlant(); this.handleSend(finalTranscript);}} className = "btn btn-success" type="button">Stop</button>
+            </div>
+          </div>
           <div>{finalTranscript}</div>
           <div>{this.state.sentiment}</div>
         </div>
