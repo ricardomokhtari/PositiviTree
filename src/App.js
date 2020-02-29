6033397aa43ba40d24f9f15856e7f38b43b8b8b5
Samuel D'Souza <samueldsouza@dyn3159-202.wlan.ic.ac.uk>
2020-02-29 12:26:47 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AudioRecorder from './components/AudioRecorder.js';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <AudioRecorder/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/AudioRecorder.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { ReactMic } from 'react-mic';
+
+export class AudioRecorder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      record: false
+    }
+
+  }
+
+  startRecording = () => {
+    this.setState({
+      record: true
+    });
+  }
+
+  stopRecording = () => {
+    this.setState({
+      record: false
+    });
+  }
+
+  onData(recordedBlob) {
+    console.log('chunk of real-time data is: ', recordedBlob);
+  }
+
+  onStop(recordedBlob) {
+    console.log('recordedBlob is: ', recordedBlob);
+  }
+
+  render() {
+    return (
+      <div>
+        <ReactMic
+          record={this.state.record}
+          className="sound-wave"
+          onStop={this.onStop}
+          onData={this.onData}
+          strokeColor="#000000"
+          backgroundColor="#FF4081" />
+        <button onTouchTap={this.startRecording} type="button">Start</button>
+        <button onTouchTap={this.stopRecording} type="button">Stop</button>
+      </div>
+    );
+  }
+}
+
+export default AudioRecorder;
