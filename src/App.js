084ec543c9aec09b04e79772f4bc5628079d4cbe
Ricardo Mokhtari <ricardomokhtari@gmail.com>
2020-02-29 14:52:51 +0000

    json

--- src/App.js
+++ src/App.js
@@ -5,41 +5,7 @@ import Stage1 from './stage 1.jpg';
 import Stage2 from './stage 2.jpg';
 import PropTypes from "prop-types";
 import SpeechRecognition from "react-speech-recognition";
-/*
-function App() {
-  const propTypes = {
-    // Props injected by SpeechRecognition
-    transcript: PropTypes.string,
-    resetTranscript: PropTypes.func,
-    browserSupportsSpeechRecognition: PropTypes.bool
-  };
 
-  const Dictaphone = ({
-    transcript,
-    resetTranscript,
-    browserSupportsSpeechRecognition
-  }) => {
-    if (!browserSupportsSpeechRecognition) {
-      return null;
-    }
-  };
-
-  Dictaphone.propTypes = propTypes;
-
-  return (
-    <React.Fragment>
-      <div className = "global">
-          <img className = "images" src = {Stage1}></img>
-          <Example/>
-          <button >Reset</button>
-          <span>{transcript}</span>
-      </div>
-    </React.Fragment>
-  );
-}
-
-export default App;
-*/
 const options = {
   autoStart: false,
   continuous: false
@@ -50,7 +16,7 @@ class Dictaphone extends Component {
   handleSend(transcript) {
     var xhr = new XMLHttpRequest()
     xhr.open('POST', 'http://localhost:3030/')
-    xhr.send(transcript) // recordedBlob is string that should be analysed
+    xhr.send(JSON.stringify(transcript)) // recordedBlob is string that should be analysed
   }
 
   render() {
